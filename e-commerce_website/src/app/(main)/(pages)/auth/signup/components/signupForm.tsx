cb7b3eec461d2a9c090bcaffdb6e4646ea95ce03
Gowthamwebnox <gowthamwebnox@gmail.com>
2025-06-17 17:58:56 +0530

    Login

--- e-commerce_website/src/app/(main)/(pages)/auth/signup/components/signupForm.tsx
+++ e-commerce_website/src/app/(main)/(pages)/auth/signup/components/signupForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "@/components/ui/input";
 
@@ -9,31 +9,85 @@ import {
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import { Button } from "@/components/ui/button";
+import axios from 'axios';
 
 const SignupForm = () => {
-  const [value, setValue] = React.useState("");
+  const [values, setValue] = React.useState("");
+  const [signupDetails, setSignupDetails]=useState({
+    name:'',
+    email:'',
+    password:'',
+    otp:'',
+  })
+  const setData=(event:any)=>{
+    if(event.target.name==='otp'){
+        console.log(values)
+        setSignupDetails({...signupDetails,[event.target.name]:values})
+    }
+    else{
+    setSignupDetails({...signupDetails,[event.target.name]:event.target.value})
+
+    }
+  }
+
+  const newUser=()=>{
+    console.log(values)
+    console.log(signupDetails)
+  }
+  const optGenerate=async()=>{
+    const payload={
+        method:'post',
+        url:'http://localhost:8000/email/verification',
+        data:{name:signupDetails.name,
+            email:signupDetails.email}
+    }
+    const response = await axios(payload)
+
+  }
+
   return (
-    <div className="border-2 border-black p-[5%] w-[40%] bg-gray-100 ">
+    <div className="border-2 border-black rounded-[12px] ml-[31%] mt-[12%] p-[5%] w-[40%] bg-gray-100 ">
       <div className="grid w-[100%] items-center gap-3">
         <Input
           type="Name"
           id="Name"
           placeholder="Name"
+          name="name"
+          value={signupDetails.name}
           className="text-center p-5 border-2 border-black"
+          onChange={(e)=>setData(e)}
         />
       </div>
+      
       <div className="grid mt-5 w-[100%] items-center gap-3 ">
         <Input
           type="email"
           id="email"
+          
+          name="email"
+          value={signupDetails.email}
           placeholder="Email"
           className="text-center  p-5 border-2 border-black"
+          onChange={(e)=>setData(e)}
         />
       </div>
-        <div className="mt-5  ml-16 flex items-center space-x-4 w-full">
+      <div className="grid mt-5 w-[100%] items-center gap-3 ">
+        <Input
+          type="Password"
+          id="Password"
+          
+          name="password"
+          value={signupDetails.password}
+          placeholder="Enter Your Password"
+          className="text-center  p-5 border-2 border-black"
+          onChange={(e)=>setData(e)}
+        />
+      </div>
+        <div className="mt-5  ml-16 md:flex items-center space-x-4 w-auto">
           <InputOTP
             maxLength={6}
-            value={value}
+            name="otp"
+            value={values}
             onChange={(value) => setValue(value)}
           >
             <InputOTPGroup className="flex">
@@ -46,12 +100,12 @@ const SignupForm = () => {
             </InputOTPGroup>
           </InputOTP>
 
-          <Button className="border-2 border-black">OTP Verification</Button>
+          <Button name="otp" onClick={(e)=>{setData(e); optGenerate()}} className="border-2 border-black">Click To Get OTP</Button>
         </div>
       
 
       <div className="mt-9 ml-[38%]">
-        <Button variant="outline" className="px-7 py-5 text-[18px] border-2 border-black" >SignUp</Button>
+        <Button variant="outline" className="px-7 py-5 text-[18px] border-2 border-black"  onClick={newUser}>SignUp</Button>
       </div>
     </div>
   );
